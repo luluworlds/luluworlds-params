4fe9dbeccb64d8a4057efa07532e1967e86ce929
ChillerDragon <chillerdragon@gmail.com>
2026-03-10 15:00:49 +0100

    Basic impl
    From https://github.com/DDNetPP/plugins/blob/43ba783577bcab7913fb0b699c828856500a3685/argparser.lua

--- README.md
+++ README.md
@@ -7,3 +7,22 @@ https://github.com/ddnet/ddnet/blob/4ae9ce874b8961ee1be4d54359997474a80db648/src
 - `s[tuning] ?f[value]`
 - `s[name] r[command]`
 
+## installation
+
+```
+luarocks install luluworlds-params
+```
+
+## example usage
+
+```lua
+local console = require("luluworlds.params")
+
+local err, params = console.parse_params("i[client id]?s[name]")
+if err == nil then
+	print(params[1].name) -- => client id
+	print(params[1].type) -- => i
+
+	print(params[2].optional) -- => true
+end
+```
--- src/params.lua
+++ src/params.lua
@@ -1,5 +1,82 @@
-local function parse_params()
-	return { foo = "bar" }
+---@class ParsedParam
+---@field name string|false the name of the param
+---@field type string the type of the param, possible values "i", "r", "s"
+---@field optional boolean
+
+---Parses a teeworlds or ddnet rcon or chat command parameter string
+---into a lua table representing the individual parameters.
+---It does NOT parse user given arguments. Only the expected parameter text.
+---
+---@param params string # teeworlds console params text like "i[client_id]"
+---@return string|nil, ParsedParam[] # error msg or nil followed by list of parsed params
+local function parse_params(params)
+   ---@type ParsedParam[]
+   local pparams = {}
+
+   ---@type string|false
+   local current_name = false
+
+   ---@type string|false
+   local current_type = false
+
+   ---@type boolean
+   local current_optional = false
+
+   -- TODO: error if optional is followed by non optional
+
+   for i = 1, #params do
+      local c = params:sub(i,i)
+      local last = params:sub(i+1,i+1) == ""
+      if current_name then
+         if c == "]" then
+            table.insert(pparams, {
+               name = current_name,
+               type = current_type,
+               optional = current_optional
+            })
+            current_name = false
+            current_type = false
+            current_optional = false
+         else
+            current_name = current_name .. c
+         end
+      elseif c == "[" then
+         current_name = ""
+      elseif c == "?" then
+         current_optional = true
+      elseif (c == "i") or (c == "s") then
+         if current_type then
+            table.insert(pparams, {
+               name = current_name,
+               type = current_type,
+               optional = current_optional
+            })
+            current_name = false
+            current_type = false
+            current_optional = false
+         end
+         -- only flush prev type and queue next
+         -- unless its end of string then we flush both
+         current_type = c
+         if last then
+            table.insert(pparams, {
+               name = current_name,
+               type = current_type,
+               optional = current_optional
+            })
+            current_name = false
+            current_type = false
+            current_optional = false
+         end
+      else
+         return "unsupported parameter type '" .. c .. "'", {}
+      end
+   end
+
+   if current_optional or current_name or current_type then
+      return "unexpected end of parameters", {}
+   end
+   return nil, pparams
 end
 
 return {
